82e50e64ee5e6fa6c25669c7603f10dce930dee5
GDROM <Rommadon.t@ku.th>
2018-02-08 11:09:53 +0700

    Do practice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -10,8 +10,12 @@ public class SetPractice {
 	 * @parameter values zero or more Strings to add to set
 	 */
 	public static void addAll(Set<String> set, String ...values) {
-		// add all the values to the set. 
-		// print each one as you add it
+		System.out.print("addAll: ");
+		  for(String arg: values) {
+		       System.out.print(arg+" ");
+		       set.add(arg);
+		    }
+			System.out.print("\n");
 	}
 	
 	/**
@@ -20,6 +24,12 @@ public class SetPractice {
 	public static void print(Set<String> set) {
 		// create an Iterator using set.iterator()
 		// then use the iterator to print everything in the set
+		Iterator<String> iter = set.iterator();
+		System.out.print("print: ");
+		while( iter.hasNext() ) {
+			System.out.print(iter.next()+" ");
+		}
+		System.out.print("\n");
 	}
 	
 	/**
@@ -27,12 +37,13 @@ public class SetPractice {
 	 */
 	public static void setExplorer( ) {
 		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
-		
+		HashSet<String> set = new HashSet<String>();
+		addAll(set, "dog", "ant", "bird","elephant","cat");
 		
 		// 2. Print elements in the set using an Iterator over the Set.
 		// You're going to need to do this again, so write a method print(Set set) to do it.
 		
-		
+		print(set);
 		// 3. are the elements printed in same order than you added them? 
 		// What does this tell you about Sets?
 		
@@ -40,28 +51,32 @@ public class SetPractice {
 		// 4. Remove all the elements from the set, using a method that 
 		// defined in Set (and Collection). What is the method to use?
 		
-		
+		set.clear();
 		// 5. Add same elements as exercise 1, but in a different order,
 		// such as: "cat", "elephant", "bird", "ant", "dog"
-		
+		addAll(set, "cat", "elephant", "bird","ant","dog");
 		
 		// 6. Print the elements in the set.  Is the order same as before?
-		
+		print(set);
 		
 		// 7. Add duplicate elements to the set.  Add another "cat" and "dog".
 		// To ensure that these are *distinct* objects use 'set.add(new String("cat"))'
 		// and new String("dog").
 		System.out.println("Add duplicate items to the set ");
-		
+		set.add(new String("cat"));
+		set.add(new String("dog"));
 		
 		// 8. Print the elements in set.  Are there any duplicates?
 		// What property of Set does this demonstrate?
 		System.out.println("Set contains: ");
+		print(set);
 		
 		
 		// 9. Repeat the exercises using a TreeSet instead of HashSet.
 		//  What is different when you use TreeSet?
-		
+	     Set<String> tset = new TreeSet<String>(set);
+	     print(tset);
+
 		// Hint: Use Polymorphism.  Make the Set object be a *parameter* to the
 		// method so you can easily use a different set just by changing the parameter!
 		
